d09b699fe17a1364b9c7cec4a7f4a072a448435e
Łukasz Janas <wookiepl@outlook.com>
2016-09-11 17:15:58 +0200

    obliczanie Root Vectora

---
+++ query-quality/src/pl/polsl/hdised/sourceIqScores/IqScoreRootVector.java
@@ -0,0 +1,46 @@
+package pl.polsl.hdised.sourceIqScores;
+
+import pl.polsl.hdised.queryPlan.Join;
+
+import java.util.List;
+
+/**
+ * Created by Wookie on 2016-09-11.
+ */
+public class IqScoreRootVector {
+
+    private static IqScoreRootVector instance = null;
+
+    private IqScoreRootVector() {}
+
+    public static IqScoreRootVector getInstance() {
+        if (instance == null) {
+            instance = new IqScoreRootVector();
+        }
+        return instance;
+    }
+
+    public IqScoreVector calculateRootResultVector(List<Join> listOfJoins) {
+
+        Integer believability = 0;
+        Integer objectivity = 0;
+        Integer reputation = 0;
+        Integer verifiability = 0;
+
+        for (Join join : listOfJoins) {
+            IqScoreVector iqScoreVector = join.calculateResultVector();
+            believability += iqScoreVector.getBelievability();
+            objectivity += iqScoreVector.getObjectivity();
+            reputation += iqScoreVector.getReputation();
+            verifiability += iqScoreVector.getVerifiability();
+        }
+
+        believability /= listOfJoins.size();
+        objectivity /= listOfJoins.size();
+        reputation /= listOfJoins.size();
+        verifiability /= listOfJoins.size();
+
+        return new IqScoreVector("root", believability, objectivity, reputation, verifiability);
+    }
+
+}
